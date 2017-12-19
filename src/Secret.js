536df77dc9f7be0f824c03a569a11a689b127e06
samjmckenzie <sammckenzies.p@gmail.com>
2017-12-19 20:59:10 +0100

    Forgot to actually change the .secret location... fixed that

--- src/Secret.js
+++ src/Secret.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const crypto = require('crypto');
 
-const path = __dirname + '/.secret';
+const path = __dirname + '/../.secret';
 
 if(fs.existsSync(path)){
     return module.exports = fs.readFileSync(path, 'utf8');
